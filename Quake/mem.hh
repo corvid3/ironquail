4266a177d94bbfc5cfb134670f77e9fa4b48d09c
korvo <128074105+corvid3@users.noreply.github.com>
2024-08-17 18:45:51 -0400

    adds a new interface to allocation engines

--- Quake/mem.hh
+++ Quake/mem.hh
@@ -4,6 +4,20 @@
 #include "common.hh"
 #include "sys.hh"
 #include <array>
+#include <cstdint>
+
+/*
+
+ideas to ponder:
+
+  - storing the destructor function associated with
+    a pointer, and then check if the pointer is free'd by the
+    destruction of an engine
+    if the pointer hasn't been freed, then call
+    the destructor to ensure proper cleanup of resources
+    in case a longjmp is called (see _Host_Frame)
+
+*/
 
 namespace QMem {
 void*
@@ -56,6 +70,76 @@ public:
   void deallocate(void* ptr);
 };
 
+template<typename Engine>
+class AllocEngineTraits
+{
+  Engine& m_engine;
+
+public:
+  AllocEngineTraits(Engine& engine)
+    : m_engine(engine)
+  {
+  }
+
+  inline void* allocate(std::size_t const bytes)
+  {
+    return m_engine.allocate(bytes);
+  }
+
+  inline void deallocate(void* ptr) { m_engine.deallocate(ptr); }
+};
+
+// wrapper that converts any
+// allocation engine to an arena
+template<typename Engine>
+class ArenaEngine
+{
+  AllocEngineTraits<Engine>& m_engine;
+
+  void* m_ptr;
+
+  // current top of the allocation
+  // points to uninitialized data
+  std::uint32_t m_offset;
+  std::uint32_t m_amt_allocated;
+
+  std::uint32_t constexpr static default_alloc_size = kibi(32);
+  float constexpr static alloc_grow_factor = 1.75f;
+
+public:
+  ArenaEngine(Engine& engine)
+    : m_engine(engine)
+  {
+    m_ptr = m_engine.allocate(default_alloc_size);
+    m_amt_allocated = default_alloc_size;
+    m_offset = 0;
+  }
+
+  ~ArenaEngine() { m_engine.deallocate(m_ptr); }
+
+  void* allocate(std::size_t const bytes)
+  {
+    if (m_offset + bytes >= m_amt_allocated) {
+      void* old_ptr = m_ptr;
+      std::uint32_t old_size = m_amt_allocated;
+
+      m_amt_allocated *= alloc_grow_factor;
+      m_ptr = m_engine.allocate(m_amt_allocated);
+
+      __builtin_memcpy(m_ptr, old_ptr, old_size);
+
+      m_engine.deallocate(m_ptr);
+    }
+
+    void* out = reinterpret_cast<char*>(m_ptr) + m_offset;
+    m_offset += bytes;
+    return out;
+  }
+
+  // do jack shit . it don't matter!
+  void deallocate(void*) {}
+};
+
 template<typename T>
 class QStackAlloc
 {
@@ -66,6 +150,12 @@ public:
 
   QStackAlloc(StackEngine& engine)
     : m_engine(engine)
+  {
+  }
+
+  template<typename O>
+  QStackAlloc(QStackAlloc<O> const& rhs)
+    : m_engine(rhs.m_engine)
   {
   }
 
